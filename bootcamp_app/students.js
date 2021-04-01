c629fb820eea550b98b5366ee60c6d787c3fb643
Dennis Wong <denniswong0106@gmail.com>
2021-04-01 03:07:55 +0000

    add new query in studnet.js file

---
+++ bootcamp_app/students.js
@@ -0,0 +1,29 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+let entry = '%' + process.argv[2] + '%';
+let entry2 = process.argv[3];
+
+console.log(entry)
+
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, [entry, entry2])
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
+
+
